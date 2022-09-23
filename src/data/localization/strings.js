773c96139dd807bd569ce6e4c8ef5eb9010a6e3e
Naji El Chemaly <najielchemaly@gmail.com>
2022-09-24 01:33:59 +0300

    fix: Remove react-localization

--- src/data/localization/strings.js
+++ src/data/localization/strings.js
@@ -1,39 +1,34 @@
-import LocalizedStrings from "react-localization";
-
-export default new LocalizedStrings({
-  en: {
-    common: { title: "Naji El Chemaly Portfolio" },
-    home: { greeting: "Hi, I am" },
-    about: {
-      iam: "I am",
-      fullname: "Full Name",
-      age: "Age",
-      years: (age) => `${age} Years`,
-      phone: "Phone",
-      nationality: "Nationality",
-      languages: "Languages",
-      email: "Email",
-      address: "Address",
-      freelance: "Freelance",
-      downloadCV: "Download CV",
-    },
-    contact: {
-      getInTouch: "Get In Touch",
-      enterYourName: "Enter Your Name*",
-      enterYourEmail: "Enter Your Email*",
-      enterYourSubject: "Enter Your Subject*",
-      enterYourMessage: "Enter Your Message*",
-      sendEmail: "Send Email",
-      validator: (field) => `${field} is required`,
-      messageSent: "Your message has been sent successfully",
-    },
-    header: {
-      home: "Home",
-      about: "About",
-      resume: "Resume",
-      portfolios: "Portfolios",
-      contact: "Contact",
-    },
+export default {
+  common: { title: "Naji El Chemaly Portfolio" },
+  home: { greeting: "Hi, I am" },
+  about: {
+    iam: "I am",
+    fullname: "Full Name",
+    age: "Age",
+    years: (age) => `${age} Years`,
+    phone: "Phone",
+    nationality: "Nationality",
+    languages: "Languages",
+    email: "Email",
+    address: "Address",
+    freelance: "Freelance",
+    downloadCV: "Download CV",
   },
-  ar: {},
-});
+  contact: {
+    getInTouch: "Get In Touch",
+    enterYourName: "Enter Your Name*",
+    enterYourEmail: "Enter Your Email*",
+    enterYourSubject: "Enter Your Subject*",
+    enterYourMessage: "Enter Your Message*",
+    sendEmail: "Send Email",
+    validator: (field) => `${field} is required`,
+    messageSent: "Your message has been sent successfully",
+  },
+  header: {
+    home: "Home",
+    about: "About",
+    resume: "Resume",
+    portfolios: "Portfolios",
+    contact: "Contact",
+  },
+};
